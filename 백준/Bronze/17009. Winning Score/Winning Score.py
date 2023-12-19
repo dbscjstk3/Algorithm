185d6182665fa43cba8909953ad9fe20c0028165
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2023-12-19 17:23:01 +0900

    [Bronze IV] Title: Winning Score, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,11 @@
+apple = [int(input()) for _ in range(3)]
+banana = [int(input()) for _ in range(3)]
+a = apple[0]*3 + apple[1]*2 + apple[2]*1
+b = banana[0]*3 + banana[1]*2 + banana[2]*1
+
+if a == b:
+    print('T')
+elif a > b:
+    print('A')
+else:
+    print('B')
