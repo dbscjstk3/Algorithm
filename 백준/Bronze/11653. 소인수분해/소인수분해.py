5ee9ff6e0668ea7a529d0691ee6da6f834722f7d
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2023-12-20 20:43:47 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,8 +1,8 @@
 N = int(input())
-i = 2
-while (N != 1):
-    if N % i == 0:
+for i in range(2,int(N**0.5)+1):
+    while N % i == 0:
         print(i)
         N //= i
-    else:
-        i += 1
+
+if N > 1:
+    print(N)
