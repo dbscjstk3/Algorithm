bcfefbbd9593eed598643889566d54a66ef859f2
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2023-12-23 17:41:40 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,12 +1,4 @@
+yoot = ['D','C','B','A','E']
 for _ in range(3):
-    yoot = list(map(int,input().split()))
-    if yoot.count(1) == 4:
-        print("E")
-    elif yoot.count(1) == 3:
-        print("A")
-    elif yoot.count(1) == 2:
-        print("B")
-    elif yoot.count(1) == 1:
-        print("C")
-    elif yoot.count(1) == 0:
-        print("D")
+    arr = list(map(int,input().split()))
+    print(yoot[sum(arr)])
