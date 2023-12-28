9a04c4eee6e35fe37508a116bcb181e76bb41eb5
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2023-12-28 12:47:08 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 416 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,23 @@
+import math
+
+def isPriem(x):
+    for i in range(2, int(math.sqrt(x)+1)):
+        if x % i == 0:
+            return False
+    return True
+
+n = int(input())
+min_value = 0
+
+for i in range(n, 1000001):
+    if i == 1:
+        continue
+    if str(i) == str(i)[::-1]:
+        if isPriem(i):
+            min_value = i
+            break
+
+if min_value == 0:
+    min_value = 1003001
+
+print(min_value)
