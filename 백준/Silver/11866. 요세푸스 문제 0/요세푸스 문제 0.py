a9df20a38bd58e26db28402b64c89e04c76064de
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2024-01-09 13:50:57 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 92 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+answer = []
+
+N, K = map(int,input().split())
+queue = deque([i for i in range(1,N+1)])
+
+while queue:
+    for i in range(K-1):
+        queue.append(queue.popleft())
+    answer.append(queue.popleft())
+
+print("<",end ='')
+for i in range(len(answer)-1):
+    print("%d, "%answer[i],end='')
+print(answer[-1],end='')
+print(">",end='')
