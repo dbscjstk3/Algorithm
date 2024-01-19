87a724b746dd8917e6229285318230c9fc17100c
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2024-01-19 14:38:41 +0900

    [level 1] Title: 시저 암호, Time: 1.97 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s,n):
+    answer = ''
+    for i in s:
+        if i.isspace():
+            answer += ' '
+        if i.isupper():
+            answer += chr((ord(i)-65+n)%26 + 65)
+        elif i.islower():
+            answer += chr((ord(i)-97+n)%26 + 97)
+    return answer
