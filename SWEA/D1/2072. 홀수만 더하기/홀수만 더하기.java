3f3638483fef82c8a4ec361ae71a03bde493e4db
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-02-13 16:17:57 +0900

    [D1] Title: 홀수만 더하기, Time: 145 ms, Memory: 20,956 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int[] nums = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int total = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] % 2 != 0) total += nums[i];
+            }
+
+            System.out.printf("#%d %d\n", tc, total);
+        }
+    }
+}
