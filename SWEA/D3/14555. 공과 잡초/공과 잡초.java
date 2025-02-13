2774cc8daf97e8678af9503830094c37ade05249
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-02-14 00:11:18 +0900

    [D3] Title: 공과 잡초, Time: 99 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			String input = br.readLine();
+			int cnt = 0;
+
+			for (int i = 1; i < input.length(); i++) {
+				if (input.charAt(i) == '|') {
+					if (input.charAt(i-1) == '(') cnt++;
+				}
+				else if (input.charAt(i) == ')') {
+					if (input.charAt(i-1) == '(' || input.charAt(i-1) == '|') cnt++;
+				}
+			}
+			System.out.printf("#%d %d\n", tc, cnt);
+		}
+	}
+}
