c9a7dafd31003b7bdb719ffa89e526a0b0524fbe
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-02-17 21:31:37 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 80 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,48 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            int[][] puzzle = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    puzzle[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			int[][] puzzle = new int[N][N];
 
-            int rCount = countSpace(puzzle, N, K, true);
-            int cCount = countSpace(puzzle, N, K, false);
-            System.out.printf("#%d %d\n", tc, rCount+cCount);
-        }
-    }
+			//배열 입력력
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					puzzle[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-    static int countSpace(int[][] puzzle, int N, int K, boolean isRow) {
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            int len = 0;
-            for (int j = 0; j < N; j++) {
-                int current = isRow ? puzzle[i][j] : puzzle[j][i];
-                if (current == 1) len++;
-                else {
-                    if (len == K) {
-                        count++;
-                    }
-                    len = 0;
-                }
-            }
+			int ans = 0;
+			for (int i = 0; i < N; i++) {
+				int countRow = 0;
+				int countCol = 0;
+				for (int j = 0; j < N; j++) {
+					//가로 탐색
+					if (puzzle[i][j] == 1) countRow++;
+					else {
+						if (countRow == K) ans++;
+						countRow = 0;
+					}
+					
+					//세로 탐색
+					if (puzzle[j][i] == 1) countCol++;
+					else {
+						if (countCol == K) ans++;
+						countCol = 0;
+					}
+				}
 
-            if (len == K) count++;
-        }
-        return count;
-    }
+				if (countRow == K) ans++;
+				if (countCol == K) ans++;
+			}
+			System.out.printf("#%d %d\n", tc, ans);
+		}
+	}
 }
