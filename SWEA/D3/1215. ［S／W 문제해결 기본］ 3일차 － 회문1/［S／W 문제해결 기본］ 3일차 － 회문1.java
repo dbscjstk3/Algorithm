54574a3168652bc174c85f4c0fedf5175dabe913
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-02-17 22:07:53 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 83 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,54 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        for (int T = 1; T <= 10; T++) {
-            int len = Integer.parseInt(br.readLine());
+	static char[][] grid = new char[8][8];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb;
+		StringBuilder sb2;
 
-            char[][] grid = new char[8][8];
-            for (int i = 0; i < 8; i++) {
-                String str = br.readLine();
-                for (int j = 0; j < 8; j++) {
-                    grid[i][j] = str.charAt(j);
-                }
-            }
+		for (int tc = 1; tc <= 10; tc++) {
+			int K = Integer.parseInt(br.readLine());
+			for (int i = 0; i < 8; i++) {
+				grid[i] = br.readLine().toCharArray();
+			}
 
-            int N = grid.length;
-            int cnt = 0;
-            //가로(행) 기준 회문 탐색
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N-len+1; j++) {
-                    boolean flag = true;
-                    for (int k = 0; k < len/2; k++) {
-                        if (grid[i][j+k] != grid[i][j-k+len-1]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) cnt++;
-                }
-            }
 
-            //세로(열) 기준 회문 탐색
-            for (int i = 0; i < N-len+1; i++) {
-                for (int j = 0; j < N; j++) {
-                    boolean flag = true;
-                    for (int k = 0; k < len/2; k++) {
-                        if (grid[i+k][j] != grid[i-k+len-1][j]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) cnt++;
-                }
-            }
+			int cnt = 0;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j <= 8-K; j++) {
+					sb = new StringBuilder();
+					sb2 = new StringBuilder();
+					for (int k = 0; k < K; k++) {
+						sb.append(grid[i][j+k]);
+					}
 
-            System.out.printf("#%d %d\n", T, cnt);
-            
-        }
-    }
+					for (int k = 0; k < K; k++) {
+						sb2.append(grid[j+k][i]);
+					}
+
+					if (sb.toString().equals(sb.reverse().toString())) cnt++;
+					if (sb2.toString().equals(sb2.reverse().toString())) cnt++;
+
+				}
+			}
+
+			System.out.printf("#%d %d\n", tc, cnt);
+		}
+	}
 }
