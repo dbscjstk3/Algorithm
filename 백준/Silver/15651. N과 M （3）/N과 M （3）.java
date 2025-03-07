3bdee99ace5dc2a84c36cca6d61222290a73a583
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-03-07 16:23:09 +0900

    [Silver III] Title: N과 M (3), Time: 352 ms, Memory: 112048 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        arr = new int[M];
+
+        permutation(0);
+        System.out.print(sb.toString()); // 한 번에 출력하여 속도 최적화
+    }
+
+    static void permutation(int depth) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n"); // 줄바꿈 추가
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            permutation(depth + 1);
+        }
+    }
+}
