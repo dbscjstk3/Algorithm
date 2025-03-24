8e3cb9679d1ad3fd1d5b86119c51ec953dffb77c
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-03-24 17:46:05 +0900

    [Silver II] Title: N과 M (11), Time: 300 ms, Memory: 106780 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static StringBuilder sb;
+	static int[] nums, ans;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+		ans = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		perm(0);
+		System.out.println(sb);
+		
+	}
+	
+	static void perm(int depth) {
+		if (depth == M) {
+			for (int num : ans) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		
+		int before = 0;
+		for (int i = 0; i < N; i++) {
+			if (before != nums[i]) {
+				ans[depth] = nums[i];
+				before = nums[i];
+				perm(depth+1);
+			}
+		}
+	}
+}
