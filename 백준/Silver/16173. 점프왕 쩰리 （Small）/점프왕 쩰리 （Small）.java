b3a222ddb6833af4eec10091137486579520bf0e
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-04-08 15:39:47 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -4,9 +4,7 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int[][] map;
-	static boolean[][] visited;
-	static int[] dr = {1,0};
-	static int[] dc = {0,1};
+	static boolean visit = false;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +12,6 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		
 		map = new int[N][N];
-		visited = new boolean[N][N];
 		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,43 +20,23 @@ public class Main {
 			}
 		}
 		
-		bfs(0,0);
+		dfs(0,0);
+		if (!visit) System.out.println("Hing");
 	}
 	
-	static void bfs(int r, int c) {
-		Queue<Pos> q = new LinkedList<>();
-		q.add(new Pos(r,c));
-		
-		while (!q.isEmpty()) {
-			Pos curr = q.poll();
-			
-			if (curr.x == N-1 && curr.y == N-1) {
-				System.out.println("HaruHaru");
-				return;
-			}
-			
-			for (int d = 0; d < 2; d++) {
-				int nr = curr.x + (dr[d] * map[curr.x][curr.y]);
-				int nc = curr.y + (dc[d] * map[curr.x][curr.y]);
-				
-				if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-				
-				if (visited[nr][nc]) continue;
-				
-				q.add(new Pos(nr,nc));
-				visited[nr][nc] = true;
-			}
+	static void dfs(int x, int y) {
+		if (map[x][y] == -1) {
+			System.out.println("HaruHaru");
+			visit = true;
 		}
-		
-		System.out.println("Hing");
-	}
-	
-	static class Pos {
-		int x, y;
-
-		public Pos(int x, int y) {
-			this.x = x;
-			this.y = y;
+		else {
+			int n = map[x][y];
+			if (n == 0)
+				return;
+			if (n + x < N) 
+				dfs(x+n, y);
+			if (n + y < N)
+				dfs(x, y+n);
 		}
 	}
 }
