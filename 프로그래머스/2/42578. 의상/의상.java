081d0386508583ca48c4ab058b8696a2bcc9afea
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-04-28 23:48:18 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int ans = 1;
+        
+        for (int i = 0; i < clothes.length; i++) {
+            String key = clothes[i][1];
+            map.put(key, map.getOrDefault(key, 0) +1);
+        }
+        
+        for (String key : map.keySet()) {
+            ans *= (map.get(key) +1);
+        }
+        
+        return ans-1;
+    }
+}
