54c25cc619e54883ddc8d91dc3f65d4e414e25d2
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2025-05-05 20:10:28 +0900

    [level 1] Title: 모의고사, Time: 0.95 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        int[] supo1 = {1,2,3,4,5};
+        int[] supo2 = {2,1,2,3,2,4,2,5};
+        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] scores = {0,0,0};
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == supo1[i%5]) scores[0]++;
+            if (answers[i] == supo2[i%8]) scores[1]++;
+            if (answers[i] == supo3[i%10]) scores[2]++;
+        }
+
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == max)
+                list.add(i+1);
+        }
+
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+    }
+}
