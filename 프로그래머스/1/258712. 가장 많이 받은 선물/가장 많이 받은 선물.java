9f5ac20a8362ca1b4010ba664dcea0c1bb800ab5
dbscjstk3 <125745678+dbscjstk3@users.noreply.github.com>
2026-02-10 22:05:16 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 15.62 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+
+        Map<String, Integer> map = new HashMap<>();
+        
+        int len = friends.length;
+
+        for (int i = 0; i < len; i++) {
+            map.put(friends[i], i);
+        }
+
+        int[][] table = new int[len][len];
+
+        int[] giftScore = new int[len];
+
+        for (String gift : gifts) {
+            String[] name = gift.split(" ");
+            String giver = name[0];
+            String getter = name[1];
+
+            giftScore[map.get(giver)]++;
+            giftScore[map.get(getter)]--;
+            table[map.get(giver)][map.get(getter)]++;
+        }
+
+        int[] giftCount = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i == j) continue;
+                if (table[i][j] > table[j][i]) giftCount[i]++;
+                else if (table[i][j] == table[j][i]) {
+                    if (giftScore[i] > giftScore[j]) giftCount[i]++;
+                }
+            }
+        }
+
+        for (int num : giftCount) {
+            answer = Math.max(answer, num);
+        }
+        
+        return answer;
+    }
+}
